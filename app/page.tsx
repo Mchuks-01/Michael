3660514657ee575e4dbd6431dcb82df2a1c343a2
Michael <fittight.20@gmail.com>
2025-03-18 09:15:02 +0000

    Refactor: Removed old pages and added new page structure

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+﻿export default function Home() {
+  return (
+    <main>
+      <h1>JESUS remains the reason for the Pain and Sacrifice!</h1>
+    </main>
+  );
+}
--- pages/index.js
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <h1>JESUS remains the reason for the Pain and Sacrifice!</h1>;
-}
